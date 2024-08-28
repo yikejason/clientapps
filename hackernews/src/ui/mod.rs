c69f37b555fdf02df14ae124cc2b5cff2972c257
yikejason <18215513386@163.com>
2024-08-28 13:42:20 +0800

    feature: use async directly inside event handler

--- hackernews/src/ui/mod.rs
+++ hackernews/src/ui/mod.rs
@@ -1,14 +1,23 @@
 #![allow(non_snake_case)]
 
-mod story_comment;
-mod story_item;
+mod comment;
+mod stories;
 
+use comment::Comments;
 use dioxus::prelude::*;
-use story_item::StoryItem;
+use stories::Stories;
 
-use crate::api::get_top_stories;
+use crate::StoryData;
+
+#[derive(Debug, Clone)]
+enum CommentsState {
+    Unset,
+    Loading,
+    Loaded(StoryData),
+}
 
 pub fn App() -> Element {
+    use_context_provider(|| Signal::new(CommentsState::Unset));
     rsx! {
       main { class: "flex w-full h-full shadow-lg rounded-3xl",
           section { class: "flex flex-col w-4/12 h-full pt-3 overflow-y-scroll bg-gray-50",
@@ -16,34 +25,9 @@ pub fn App() -> Element {
           }
           section { class: "flex flex-col w-8/12 px-4 bg-white rounded-r-3xl",
               section {
-                  ul {}
+                Comments {}
               }
           }
       }
-    }
-}
-
-#[component]
-fn Stories() -> Element {
-    let stories = use_resource(move || get_top_stories(20));
-    match &*stories.read_unchecked() {
-        Some(Ok(stories)) => rsx! {
-          ul {
-            for story in stories {
-              StoryItem { story: story.clone()}
-            }
-          }
-        },
-        Some(Err(err)) => rsx! {
-          div { class: "mt-6 text-red-500" }
-          p { "Failed to fetch stories"}
-          p { "{err}" }
-        },
-        None => rsx! {
-          div {
-            class: "mt-6",
-            p { "Loading..." }
-          }
-        },
     }
 }
